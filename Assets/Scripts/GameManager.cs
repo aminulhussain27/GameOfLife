9e2eec680394390d4b21b100a6e75b04acbfd3fe
wolf <aminulhussain27@gmail.com>
2019-10-07 17:08:41 +0530

    project base

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public CellScript cellPrefab;
+
+	public float updateInterval = 0.1f;
+    float counter;
+
+    // Declaring a matrix for cells, allows neighbour checking!
+    public CellScript[,] cells;
+
+	public bool isPlaying = false;
+
+	public int generation;
+	public Text genText;
+
+	//Singleton
+	private static GameManager instance = null;
+
+	public static GameManager Instance()
+	{
+		if(instance == null)
+		{
+			instance = FindObjectOfType<GameManager> ();
+		}
+		return instance;
+	}
+
+	void Start ()
+	{
+		InitGrid ();
+    }
+
+	void Update ()
+	{
+		if(isPlaying)
+		{
+			counter += Time.deltaTime;
+			if(counter >= updateInterval)
+			{
+				counter = 0.0f;
+				UpdateCells();
+			}
+		}
+	}
+
+	public void UpdateCells()
+	{
+		for (int i = 0; i < 6; i++)
+		{
+			for (int j = 0; j < 6; j++)
+			{
+				cells[i,j].CellUpdate();
+			}
+		}
+		for (int i = 0; i < 6; i++)
+		{
+			for (int j = 0; j < 6; j++)
+			{
+				cells[i,j].ApplyCellUpdate();
+			}
+		}
+
+		generation++;
+		genText.text = generation.ToString("000");
+	}
+
+	public void RemoveGrid ()
+	{
+		// Checks if there are cells in the grid or not.
+		if (cells == null) return;
+
+		for (int i = 0; i < 6; i++)
+		{
+			for (int j = 0; j < 6; j++)
+			{
+                SpawnPoolManager.instance.Despawn (cells [i, j].gameObject);
+			}
+		}
+	}
+
+	public void InitGrid ()
+	{
+		generation = 0;
+
+		genText.text = generation.ToString("000");
+
+		//Create cells
+		cells = new CellScript[6, 6];
+
+		for (int i = 0; i < 6; i++)// 6cell size in X direction
+		{
+			for (int j = 0; j < 6; j++)//6 cells in Y directions
+			{
+				// Creating a cell into the scene.
+
+				CellScript c = SpawnPoolManager.instance.Spawn ("CellSprite", new Vector3 (i * 4.5f, j * 4.5f, 0.0f), Quaternion.identity);
+
+				c.InitCell (i, j);
+
+				cells [i, j] = c;
+			}
+		}
+	}
+
+	public void ResetCells ()
+	{
+		// Checks if there are cells in the grid or not.
+		if (cells == null) return;
+
+		generation = 0;
+		genText.text = generation.ToString("000");
+
+		for (int i = 0; i < 6; i++)
+		{
+			for (int j = 0; j < 6; j++)
+			{
+				cells [i, j].ClearCell ();
+            }
+		}
+	}
+
+	public void Run ()
+	{
+		if (!isPlaying)
+		{
+			isPlaying = true;
+			counter = 0.0f;
+        }
+	}
+
+	public void Stop ()
+	{
+		if (isPlaying)
+		{
+			isPlaying = false;
+			counter = 0.0f;
+		}
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+	[Header ("GameObject")]
+	public GameObject PlayButton;
+	public GameObject StopButton;
+	public GameObject StepButton;
+
+	public GameObject MoreSizeButton;
+	public GameObject LessSizeButton;
+
+	[Header ("Slider")]
+	public Slider timeSlider;
+
+    [Header("Text")]
+    public Text mapSizeText;
+    public Text updateIntervalText;
+
+    void Start()
+    {
+		updateIntervalText.text = "Time Step: " + Mathf.Round(GameManager.Instance().updateInterval * 1000.0f) + "ms";
+    }
+
+    public void StartSim ()
+	{
+		GameManager.Instance().Run ();
+
+		PlayButton.SetActive (false);
+		StopButton.SetActive (true);
+		StepButton.SetActive (false);
+	}
+
+	public void StopSim ()
+	{
+		GameManager.Instance().Stop ();
+
+		PlayButton.SetActive (true);
+		StopButton.SetActive (false);
+		StepButton.SetActive (true);
+	}
+
+	public void NextStep()
+	{
+		GameManager.Instance().UpdateCells();
+	}
+
+    // Clear map now automatically stops the simulation.
+	public void ClearMap ()
+	{
+        StopSim();
+		GameManager.Instance().ResetCells ();
+	}
+
+
+	public void ChangeUpdateInterval (Slider slider)
+	{
+		GameManager.Instance().updateInterval = slider.value;
+
+		updateIntervalText.text = "Time Step: " + Mathf.Round(GameManager.Instance().updateInterval * 1000.0f) + "ms";
+    }
+}
